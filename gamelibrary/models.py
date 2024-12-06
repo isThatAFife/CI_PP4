9f61ac48ead034a3c76b44098f3e65b98da06c60
D <dylankfilm@gmail.com>
2024-12-07 00:07:08 +0900

    removed genre column that was causing issues in db

--- gamelibrary/models.py
+++ gamelibrary/models.py
@@ -8,7 +8,6 @@ import datetime
 class Game(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    genre = models.CharField(max_length=100,unique=False)
     score = models.DecimalField(max_digits=2, decimal_places=1)
     year = models.PositiveIntegerField(default=1997, validators=[MinValueValidator(1920), MaxValueValidator((datetime.date.today().year+1))])
     developer = models.CharField(max_length=200, unique=False)
