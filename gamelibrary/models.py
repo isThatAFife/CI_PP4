5d11c39708fa46841c0a54228b2536910a48fb56
D <dylankfilm@gmail.com>
2024-12-10 11:49:13 +0900

    added slug field to model and began working on game_detail.html

--- gamelibrary/models.py
+++ gamelibrary/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -11,5 +12,21 @@ class Game(models.Model):
     console = models.CharField(max_length=200, default='NA')
     userscore = models.CharField(max_length=3, default='0')
     date = models.CharField(max_length=100, default='1998')
+    slug = models.SlugField(max_length=250, unique=True, blank=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            # Combine name and console, then slugify
+            slug_str = f"{self.name} {self.console}"
+            self.slug = slugify(slug_str)
+            
+            # Ensure uniqueness of the slug
+            original_slug = self.slug
+            count = 1
+            while Game.objects.filter(slug=self.slug).exists():
+                self.slug = f"{original_slug}-{count}"
+                count += 1
+                
+        super().save(*args, **kwargs)
  
     
--- gamelibrary/views.py
+++ gamelibrary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Game
 
@@ -8,3 +8,26 @@ class GameList(generic.ListView):
     template_name = "gamelibrary/index.html"
     paginate_by = 18
 
+
+def game_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Game.objects.filter(all)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
