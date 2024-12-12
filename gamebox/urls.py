53cf02d4fdca62b34a447fcea53141692baadd89
D <dylankfilm@gmail.com>
2024-12-12 13:03:15 +0900

    added authentication

--- gamebox/urls.py
+++ gamebox/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("about/", include("about.urls"), name="about-urls"),
+    path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("gamelibrary.urls"), name="gamelibrary-urls"),
