9c9d03bf9ce426694f20b7ff0ab9c163fb725fb9
D <dylankfilm@gmail.com>
2024-12-13 12:53:26 +0900

    added edit and delete buttons for comments

--- gamelibrary/urls.py
+++ gamelibrary/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("", views.GameList.as_view(), name="home"),
     path("my-comments/", views.user_comments, name="user_comments"),
     path("<slug:slug>/", views.game_detail, name="game_detail"),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+        views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+        views.comment_delete, name='comment_delete'),
 ]
--- gamelibrary/views.py
+++ gamelibrary/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.conf import settings
 from django.contrib import messages
-from .models import Game
+from .models import Game, Comment
 from .forms import CommentForm
 import requests
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import Comment
+from django.http import HttpResponseRedirect
 
 
 @login_required
@@ -169,3 +169,42 @@ def game_detail(request, slug):
             "comment_form": comment_form,
         },
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Game.objects.all()
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('game_detail', args=[slug]))
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Game.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('game_detail', args=[slug]))
